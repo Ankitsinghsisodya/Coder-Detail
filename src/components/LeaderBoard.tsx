8f7213cd8a4b363a89dbe20816ee4052b353d39f
Ankitsinghsisodya <ankitsingh24012005@gmail.com>
2025-06-07 13:12:06 +0530

    rating feature added

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -16,10 +16,12 @@ async function LeaderBoard() {
       {
         ranks?.map((user: any) => {
           return (
-            <div key={user?.id} className='grid grid-cols-4  text-center bg-gray-800 p-4 rounded-lg m-2 text-white'>
+            <div key={user?.id} className='grid grid-cols-6  text-center bg-gray-800 p-4 rounded-lg m-2 text-white'>
               <div>{user?.name}</div>
               <div>{user?.codeforcesProblemSolved}</div>
               <div>{user?.leetcodeProblemSolved}</div>
+              <div>{user.leetcodeRating}</div>
+              <div>{user.codeforcesRating}</div>
               <div>{user?.solvedProblems}</div>
             </div>
           )
